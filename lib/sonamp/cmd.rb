a4bba7d6666343c3f5a3a702905a801fe5ece337
Oleg Pudeyev <code@olegp.name>
2022-10-27 13:21:43 -0400

    restructure cmd

---
+++ lib/sonamp/cmd.rb
@@ -0,0 +1,63 @@
+require 'optparse'
+require 'logger'
+require 'sonamp/utils'
+require 'sonamp/client'
+
+module Sonamp
+  class Cmd
+    def initialize(args)
+      options = {}
+      OptionParser.new do |opts|
+        opts.banner = "Usage: sonamp [-d device] command arg..."
+
+        opts.on("-d", "--device DEVICE", "TTY to use (default autodetect)") do |v|
+          options[:device] = v
+        end
+      end.parse!(args)
+
+      @options = options
+
+      @logger = Logger.new(STDERR)
+      @client = Sonamp::Client.new(options[:device], logger: @logger)
+
+      @args = args
+    end
+
+    attr_reader :args
+
+    def run
+      cmd = args.shift
+      unless cmd
+        raise ArgumentError, "No command given"
+      end
+
+      case cmd
+      when 'off'
+        client.set_zone_power(1, false)
+        client.set_zone_power(2, false)
+        client.set_zone_power(3, false)
+        client.set_zone_power(4, false)
+      when 'power'
+        zone = args.shift.to_i
+        state = Utils.parse_on_off(ARGV.shift)
+        client.set_zone_power(zone, state)
+      when 'zvol'
+        zone = args.shift.to_i
+        volume = ARGV.shift.to_i
+        client.set_zone_volume(zone, volume)
+      when 'cvol'
+        channel = args.shift.to_i
+        volume = args.shift.to_i
+        client.set_channel_volume(channel, volume)
+      when 'status'
+        client.status
+      else
+        raise ArgumentError, "Unknown command: #{cmd}"
+      end
+    end
+
+    private
+
+    attr_reader :client
+  end
+end
