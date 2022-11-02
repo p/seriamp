b81c5944dadaa9b022700e7db096f7bbe60c1596
Oleg Pudeyev <code@olegp.name>
2022-11-02 17:35:42 -0400

    web app

---
+++ lib/yamaha/utils.rb
@@ -0,0 +1,15 @@
+module Yamaha
+  module Utils
+
+    module_function def parse_on_off(value)
+      case value&.downcase
+      when '1', 'on', 'yes', 'true'
+        true
+      when '0', 'off', 'no', 'false'
+        false
+      else
+        raise ArgumentError, "Invalid on/off value: #{value}"
+      end
+    end
+  end
+end
