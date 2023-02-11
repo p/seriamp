71b48905d626a2b8fffcae2f7362ce1c27f9e878
Oleg Pudeyev <code@olegp.name>
2023-02-11 14:28:20 -0500

    cmd test

--- spec/yamaha/cmd_spec.rb
+++ spec/yamaha/cmd_spec.rb
@@ -8,4 +8,15 @@ describe Seriamp::Yamaha::Cmd do
       described_class.new
     end
   end
+
+  let(:args) { [] }
+  let(:stdin_c) { '' }
+  let(:stdin) { StringIO.new(stdin_c) }
+  let (:cmd) { described_class.new(args, stdin) }
+
+  describe '!status' do
+    it 'works' do
+      cmd.run
+    end
+  end
 end
