5eff5462e82ec1dc2d40003f0fc7c9be9dd33e0b
Oleg Pudeyev <code@olegp.name>
2023-02-14 10:51:50 -0500

    yamaha app test

--- spec/yamaha/app_spec.rb
+++ spec/yamaha/app_spec.rb
@@ -3,9 +3,35 @@
 require 'spec_helper'
 
 describe Seriamp::Yamaha::App do
+  include Rack::Test::Methods
+
   describe '#initialize' do
     it 'works' do
       described_class.new
+    end
+  end
+
+  let(:app) do
+    described_class.tap do |app|
+      app.client = client
+    end
+  end
+
+  let(:client_cls) { Seriamp::Yamaha::Client }
+  let(:client) { double('yamaha client') }
+
+  describe 'get /power' do
+  end
+
+  describe 'put /main/power' do
+    it 'works' do
+      client.should receive(:with_device).and_yield
+      client.should receive(:set_main_power).with(true)
+
+      put '/main/power', 'true'
+
+      last_response.status.should == 204
+      last_response.body.should == ''
     end
   end
 end
