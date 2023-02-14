0a63283437d0c02af79550da8534587cb29c827a
Oleg Pudeyev <code@olegp.name>
2023-02-14 09:28:05 -0500

    sonamp auto power via network access

---
+++ lib/seriamp/faraday_facade.rb
@@ -0,0 +1,84 @@
+require 'faraday'
+autoload :JSON, 'json'
+
+module Seriamp
+  class FaradayFacade
+    class HttpError < StandardError
+    end
+
+    def initialize(**opts)
+      @options = {}
+      if timeout = opts.delete(:timeout)
+        @options[:timeout] = timeout
+      end
+
+      @conn = Faraday.new(**opts) do |faraday|
+        faraday.response :logger, nil, { headers: true, bodies: false }
+        #faraday.response :logger
+      end
+
+      @base_url = opts.fetch(:url)
+    end
+
+    attr_reader :options
+    attr_reader :base_url
+
+    def get(uri)
+      conn.get(uri) do |req|
+        configure_request(req)
+      end
+    end
+
+    def get_json(url)
+      resp = get(url)
+      if resp.status != 200
+        raise HttpError, "Bad status: #{resp.status} for #{base_url} #{url} with #{options}"
+      end
+      JSON.parse(resp.body)
+    end
+
+    def put(uri, body: nil)
+      conn.put(uri) do |req|
+        configure_request(req)
+        if body
+          req.body = body
+        end
+      end
+    end
+
+    def put!(url, **opts)
+      put(url, **opts).tap do |resp|
+        unless resp.success?
+          raise HttpError, "Bad status: #{resp.status} for #{url}"
+        end
+      end
+    end
+
+    def post(uri, body: nil)
+      conn.post(uri) do |req|
+        configure_request(req)
+        if body
+          req.body = body
+        end
+      end
+    end
+
+    def post!(url, **opts)
+      post(url, **opts).tap do |resp|
+        unless resp.success?
+          raise HttpError, "Bad status: #{resp.status} for #{url}"
+        end
+      end
+    end
+
+    private
+
+    attr_reader :conn
+
+    def configure_request(req)
+      req.options.timeout = options[:timeout]
+      req.options.read_timeout = options[:timeout]
+      req.options.open_timeout = options[:timeout]
+    end
+  end
+end
---
+++ lib/seriamp/sonamp/auto_power.rb
@@ -0,0 +1,97 @@
+require 'seriamp/faraday_facade'
+require 'seriamp/utils'
+
+module Seriamp
+  class AutoPower
+    def initialize(**opts)
+      @options = opts.dup.freeze
+    end
+
+    attr_reader :options
+
+    def logger
+      options[:logger]
+    end
+
+    def run
+      bump('application start')
+
+      prev_sonamp_power = nil
+      handle_exceptions do
+        prev_sonamp_power = sonamp_client.get_zone_power.values.any? { |v| v == true }
+      end
+
+      loop do
+        sonamp_power = nil
+        handle_exceptions do
+          sonamp_power = sonamp_client.get_zone_power.values.any? { |v| v == true }
+          if sonamp_power && prev_sonamp_power == false
+            bump('amplifier turned on')
+          end
+          prev_sonamp_power = sonamp_power
+        end
+
+        # If we cannot query the receiver, assume it is on to prevent unintended
+        # turn-offs.
+        receiver_power = nil
+        handle_exceptions do
+          receiver_power = yamaha_client.main_power
+        end
+        p receiver_power
+        case receiver_power
+        when true
+          bump('receiver is on')
+          if sonamp_power == false
+            puts("turning on amplifier")
+            sonamp_client.set_zone_power(1, true)
+            sonamp_client.set_zone_power(2, true)
+            sonamp_client.set_zone_power(3, true)
+          end
+        when nil
+          bump('failed to communicate with receiver - assuming it is on')
+        end
+
+        delta = (@alive_through - Utils.monotime).to_i
+        if delta < 0
+          logger&.info("Turning amplifier off")
+          handle_exceptions do
+            sonamp_client.power_off
+          end
+        else
+          puts "TTL: #{delta / 60}:#{'%02d' % (delta % 60)}"
+        end
+
+        sleep 20
+      end
+    end
+
+    private
+
+    def handle_exceptions
+      yield
+    rescue Interrupt, SystemExit, NoMemoryError
+      raise
+    rescue => exc
+      logger&.warn("Unhandled exception: #{exc.class}: #{exc}")
+    end
+
+    def bump(reason)
+      logger&.debug("Bumping 7 minutes: #{reason}")
+      @alive_through = Utils.monotime + 7*60
+    end
+
+    def sonamp_client
+      @sonamp_client ||= FaradayFacade.new(
+        url: options.fetch(:sonamp_url),
+        timeout: options[:sonamp_timeout] || 3,
+      )
+    end
+
+    def yamaha_client
+      @yamaha_client ||= FaradayFacade.new(
+        url: options.fetch(:yamaha_url),
+        timeout: options[:yamaha_timeout] || 5,
+      )
+    end
+  end
+end
