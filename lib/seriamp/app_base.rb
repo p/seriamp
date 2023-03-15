d04eb9038d9d76e4c75b996a7f4e2909a77adda7
Oleg Pudeyev <code@olegp.name>
2023-03-15 17:29:53 -0400

    dry app cmd

--- lib/seriamp/app_base.rb
+++ lib/seriamp/app_base.rb
@@ -3,51 +3,49 @@
 require 'sinatra/base'
 
 module Seriamp
-  module Sonamp
-    class AppBase < Sinatra::Base
+  class AppBase < Sinatra::Base
 
-      set :device, nil
-      set :logger, nil
-      set :client, nil
-      set :retries, true
+    set :device, nil
+    set :logger, nil
+    set :client, nil
+    set :retries, true
 
-      private
+    private
 
-      def accept_json?
-        request.env['HTTP_ACCEPT'] == 'application/json'
+    def accept_json?
+      request.env['HTTP_ACCEPT'] == 'application/json'
+    end
+
+    def render_json(data)
+      headers['content-type'] = 'application/json'
+      data.to_json
+    end
+
+    def empty_response
+      if accept_json?
+        render_json({})
+      else
+        [204, '']
       end
+    end
 
-      def render_json(data)
+    def plain_response(data)
+      headers['content-type'] = 'text/plain'
+      data.to_s
+    end
+
+    def render_422(error)
+      if accept_json?
         headers['content-type'] = 'application/json'
-        data.to_json
-      end
-
-      def empty_response
-        if accept_json?
-          render_json({})
-        else
-          [204, '']
-        end
-      end
-
-      def plain_response(data)
+        [422, {error: error}.to_json]
+      else
         headers['content-type'] = 'text/plain'
-        data.to_s
+        [422, error]
       end
+    end
 
-      def render_422(error)
-        if accept_json?
-          headers['content-type'] = 'application/json'
-          [422, {error: error}.to_json]
-        else
-          headers['content-type'] = 'text/plain'
-          [422, error]
-        end
-      end
-
-      error InvalidOnOffValue do |e|
-        render_422("Error: #{e.class}: #{e}")
-      end
+    error InvalidOnOffValue do |e|
+      render_422("Error: #{e.class}: #{e}")
     end
   end
 end
---
+++ lib/seriamp/app_cmd.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require 'logger'
+
+module Seriamp
+  class AppCmd
+    def initialize(args = ARGV, stdin = STDIN, module_name: nil)
+      bin_name = if module_name
+        "#{module_name}-web"
+      else
+        'seriamp-web'
+      end
+
+      options = {module: module_name}
+      OptionParser.new do |opts|
+        if module_name
+          opts.banner = "Usage: #{bin_name} [options] [-- rackup-options]"
+        else
+          opts.banner = "Usage: #{bin_name} -m module [options] [-- rackup-options]"
+
+          opts.on("-m", "--module MODULE", "Device module to use: integra|sonamp|yamaha") do |v|
+            options[:module] = v
+          end
+        end
+
+        opts.on("-b", "--backend BACKEND", "Backend to use for communication") do |v|
+          options[:backend] = v
+        end
+
+        opts.on("-d", "--device DEVICE", "TTY to use (default autodetect /dev/ttyUSB*)") do |v|
+          options[:device] = v
+        end
+
+        opts.on('-T', '--timeout TIMEOUT', 'Timeout to use') do |v|
+          options[:timeout] = Float(v)
+        end
+
+        opts.separator ''
+        opts.separator "To see rackup options: #{bin_name} -- -h"
+      end.parse!(args)
+
+      @options = options
+
+      @mod_name = options.fetch(:module)
+
+      require "seriamp/#{mod_name}"
+      require "seriamp/#{mod_name}/app"
+
+      mod = Seriamp.const_get(mod_name.sub(/(.)/) { $1.upcase })
+      @app_mod = mod.const_get(:App)
+
+      @logger = Logger.new(STDERR)
+
+      @client = mod.const_get(:Client).new(device: options[:device],
+        logger: @logger, timeout: options[:timeout], thread_safe: true)
+
+      @app_mod.set(:client, @client)
+
+      @rack_options = Rack::Server::Options.new.parse!(args)
+    end
+
+    attr_reader :mod_name
+    attr_reader :mod
+    attr_reader :args
+    attr_reader :stdin
+    attr_reader :logger
+    attr_reader :options
+
+    def run
+      Rack::Server.start(@rack_options.merge(app: @app_mod))
+    end
+  end
+end
