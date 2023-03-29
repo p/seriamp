e01449354619c9787cf7f8bd9bd1f1b691238a50
Oleg Pudeyev <code@olegp.name>
2023-03-29 13:17:37 -0400

    test wip

--- spec/unit/sonamp/auto_power_spec.rb
+++ spec/unit/sonamp/auto_power_spec.rb
@@ -3,6 +3,7 @@
 require 'spec_helper'
 
 describe Seriamp::Sonamp::AutoPower do
+  let(:logger) { Logger.new(STDERR) }
 
   describe '#initialize' do
     context 'with yamaha detector' do
@@ -19,13 +20,35 @@ describe Seriamp::Sonamp::AutoPower do
     end
   end
 
-  let(:runner) do
-    described_class.tap do |app|
-      app.client = client
+  describe '#run' do
+    context 'with sonamp detector' do
+      let(:runner) do
+        described_class.new(sonamp_url: 'http://test/sonamp', detector: :sonamp, logger: logger)
+      end
+
+      context 'when initial connection fails' do
+        let(:conn) { double('test connection') }
+
+        before do
+          Seriamp::FaradayFacade.should receive(:new).and_return(conn)
+          conn.should_receive(:get_json).and_raise(Faraday::ConnectionFailed)
+        end
+
+        it 'stays running and retries' do
+          runner.run
+        end
+      end
+
+      context 'no input signal' do
+        it 'powers amplifier off' do
+          runner.should receive(:wait_for_next_iteration).and_throw(:finish)
+
+          catch(:finish) do
+            runner.run
+          end
+        end
+      end
     end
   end
-
-  let(:client_cls) { Seriamp::Sonamp::Client }
-  let(:client) { double('sonamp client') }
 
 end
