77c50eb6e014b56394610b2b150c6ec7d1f99f69
Oleg Pudeyev <code@olegp.name>
2023-05-29 22:25:21 -0400

    delete unused

--- lib/seriamp/yamaha/protocol/status.rb
+++ lib/seriamp/yamaha/protocol/status.rb
@@ -507,31 +507,6 @@ module Seriamp
             [2, :status_flags],
           ].freeze,
         }.freeze
-
-        STATUS_FIELDS_R0178 = [
-          [nil, nil, 'Baud rate (@)'],
-          [nil, nil, 'Receive buffer (E)'],
-          [nil, nil, 'Receive buffer (0)'],
-          [nil, nil, 'Command timeout (1)'],
-          [nil, nil, 'Command timeout (9)'],
-          [nil, nil, 'Command timeout (0)'],
-          [nil, nil, 'Handshaking (0)'],
-
-          [:busy, :off_on, 'System busy status'],
-          [:power, :off_on, 'System power'],
-          [:input, nil, 'Main zone input'],
-          [:multi_ch_input, :off_on, 'Input is multi-channel input'],
-          [:input_mode, nil, 'Input mode'],
-          [:mute, :off_on, 'Main zone mute'],
-        ].freeze
-
-        STATUS_FIELDS_R0226 = (STATUS_FIELDS_R0178[..6] + [
-        ]).freeze
-
-        STATUS_FIELDS_MAP = {
-          'R0178' => STATUS_FIELDS_R0178,
-          'R0226' => STATUS_FIELDS_R0226,
-        }.freeze
       end
     end
   end
