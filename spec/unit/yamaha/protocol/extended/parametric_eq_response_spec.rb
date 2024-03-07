9193cfe01ca82b5231d983bb0d47a3db124e4194
Oleg Pudeyev <code@olegp.name>
2024-03-07 13:07:03 -0500

    peq test

---
+++ spec/unit/yamaha/protocol/extended/parametric_eq_response_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Seriamp::Yamaha::Protocol::Extended::ParametricEqResponse do
+  let(:response) do
+    # RX-V3800
+    described_class.new('034', '332E215')
+  end
+
+  describe '#to_state' do
+    it 'returns expected result' do
+      response.to_state.should == {
+        front_right_peq_4: {frequency: '2E', gain: -3.5, q: 1.587},
+      }
+    end
+  end
+end
