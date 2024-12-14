1a7b61fd2e6d8362cae37d989ac4046906b8a8cf
Oleg Pudeyev <code@olegp.name>
2024-12-14 16:36:10 -0500

    reorder

--- lib/seriamp/yamaha/response.rb
+++ lib/seriamp/yamaha/response.rb
@@ -6,17 +6,17 @@ module Seriamp
   module Yamaha
     class Response
       include Seriamp::AsciiTable
-      
+
       def self.parse(resp, logger: nil)
         unless resp[-1] == ETX
           raise UnexpectedResponse, "Invalid response: expected to end with ETX: #{resp}"
         end
-        
+
         case first_byte = resp[0]
-        when STX
-          CommandResponse.parse(resp[1..-1], logger: logger)
         when DC2
           StatusResponse.parse(resp[1..-1], logger: logger)
+        when STX
+          CommandResponse.parse(resp[1..-1], logger: logger)
         when DC4
           ExtendedResponse.parse(resp[1..-1], logger: logger)
         else
