55ad8fb18e78384557cc8165226bae6a3920c49b
Oleg Pudeyev <code@olegp.name>
2024-12-15 18:08:41 -0500

    read buffer flushing

--- lib/seriamp.rb
+++ lib/seriamp.rb
@@ -6,4 +6,20 @@ module Seriamp
   autoload :FaradayFacade, 'seriamp/faraday_facade'
   autoload :Sonamp, 'seriamp/sonamp'
   autoload :Yamaha, 'seriamp/yamaha'
+
+  # Reads any data from the device at specified path and returns it.
+  # Intended for development/debugging.
+  module_function def flush_device_read_buffer(path)
+    contents = nil
+    begin
+      File.open(path) do |io|
+        while chunk = io.read_nonblock(1024)
+          contents ||= +''
+          contents += chunk
+        end
+      end
+    rescue IO::EAGAINWaitReadable
+    end
+    contents
+  end
 end
